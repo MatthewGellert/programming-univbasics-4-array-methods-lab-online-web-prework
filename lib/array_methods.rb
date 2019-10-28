f80473f7e289cdebc0fb3c46c0ee3015e97f3ae2
Matthew Gellert <kifireguy@gmail.com>
2019-10-28 16:55:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -32,9 +32,12 @@ def using_first do
 end
 
 def using_last do
-
+ it "takes in argument of an array and and returns the last element in the array" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_last(array)).to eq("arrays!")
+  end
 end
 
-def using_size(array)
+def using_size do
 
 end
